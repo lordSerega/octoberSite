e926c4cb6f83a639152cc0a80f32b48fa14e55b2
lordSerega <kapelka001@ya.ru>
2020-03-16 20:47:43 +0300

    REALESE
    Добавлен чат
    Добавлена форма обратной связи
    Добавлены все страницы ( нет графика приема граждан и отзывов)

---
+++ php/post.php
@@ -0,0 +1,238 @@
+
+
+<!-- Через 10 секунд после появления сообщения об отправке или ошибке — отправляемся на сайт Кода :) -->
+<meta http-equiv='refresh' content='10; url=https://каплин.рус'>
+
+<meta charset="UTF-8" />
+
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://kit.fontawesome.com/147ad66cb5.js" crossorigin="anonymous"></script>
+    <title>Стоматологическая клиника ОКТЯБРЬ в Ростове-на-Дону</title>
+    <link href="../css/index.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="sticky-top bg-white">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-xl-12 col-md-12 pl-0 pr-0 ">
+                    <nav class="navbar navbar-expand-lg navbar-light ">
+                        <a class="navbar-brand" href="#"><img src="../img/logo.png" class="logo" alt=""></a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.html">Главная</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> О клинике
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="company/about.html">Подробнее<span class="sr-only">(current)</span></a>
+                                        <a class="dropdown-item" href="company/licences.html">Лицензии и рекомендации</a>
+                                        <a class="dropdown-item" href="company/advantages.html">Преимущества клиники</a>
+                                        <a class="dropdown-item" href="company/services.html">Услуги</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="company/docs.html">Документы</a>
+                                        <a class="dropdown-item" href="company/reseption.html">График приема</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="timetable.html">Расписание</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="price.html">Прайс</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link " href="contacts.html">Контакты<span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="reviews.html">Отзывы</a>
+                                </li>
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0">
+                    <button type="button" class="btn btn-outline-dark my-2 my-sm-0" data-toggle="modal" data-target="#myModal">Записать на прием</button>
+                            </form>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 text-center">
+               <h1 class=" pt-10">Сообщение успешно отправлено. Через некоторое время свами свяжется администратор для уточнения данных.</h1>
+			   <h2>Через 10 секунд вы будете перенаправлены на главную страницу сайта.</h2>
+           
+           
+            </div>
+        </div>
+    </div>
+    
+   
+  <section class="pt-3">
+        <div class="kek bg-light fixed-bottom">
+
+            <div class="container text-dark">
+                <div class="row">
+                    <div class="col-xl-3 text-left">
+                        <h3>&#160;</h3>
+                        <h6>Клиника в Ростове-на-Дону</h6>
+                        <p><i class="fa fa-location-arrow" aria-hidden="true"></i> бульвар Комарова 12/1</p>
+                        <p><i class="fa fa-phone"></i> +7 (863) 256 08-20</p>
+                    </div>
+                    <div class="col-xl-6 text-left">
+                        <h3>&#160;</h3>
+                        <h6>Клиника в Новочеркасске</h6>
+                        <p><i class="fa fa-location-arrow" aria-hidden="true"></i> ул. Просвещения, 77б </p>
+                        <p><i class="fa fa-phone"></i> +7 (8635) 244-255</p>
+                    </div>
+                    <div class="col-xl-3 pull-right">
+                        <h3>&#160;</h3>
+                        <div id="sliderInner" class="carousel slide " data-ride="carousel" data-interval="2000">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="../img/footer/logo1.png" class="d-block w-100" alt="Природа">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="../img/footer/logo2.png" class="d-block w-100" alt="Природа">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="../img/footer/logo3.png" class="d-block w-100" alt="Природа">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="../img/footer/logo4.png" class="d-block w-100" alt="Природа">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="../img/footer/logo5.png" class="d-block w-100" alt="Природа">
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12 text-center">
+                        <br>
+                        <p class="lead">Горячая линия для приема жалоб, предложений и замечаний <br> <i class="fa fa-phone"></i> +7-991-365-44-55</p>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6">
+                        <p class="small">
+                            © 2020 Все права защищены. <br>
+                            "ОКТЯБРЬ" — Стоматология в Ростове-на-Дону <br>
+                            Разработка и продвижение сайта Студия "kaplin-web"
+                        </p>
+                    </div>
+                    <div class="col-xl-6 text-right">
+                        <p>
+                            <small><a href="police/index.html" class="text-dark">Политика конфедециальности</a></small> <br>
+                            <small><a href="" class="text-dark">Версия для слабовидящих</a></small> <br>
+                            <small><a href="" class="text-dark">Версия для печати</a></small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+<script type='text/javascript'>
+(function () {
+    window['yandexChatWidgetCallback'] = function() {
+        try {
+            window.yandexChatWidget = new Ya.ChatWidget({
+                guid: 'c994aa03-b35b-4265-87f7-455921a03dd3',
+                buttonText: '',
+                title: 'Октябрь',
+                theme: 'dark',
+                collapsedDesktop: 'never',
+                collapsedTouch: 'always'
+            });
+        } catch(e) { }
+    };
+    var n = document.getElementsByTagName('script')[0],
+        s = document.createElement('script');
+    s.async = true;
+    s.charset = 'UTF-8';
+    s.src = 'https://chat.s3.yandex.net/widget.js';
+    n.parentNode.insertBefore(s, n);
+})();
+</script>
+    </section>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+    <script src="js/recall_me.js"></script>
+</body>
+
+</html>
+
+ 
+<!-- Начался блок PHP -->
+
+<?php
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+// Получаем значения переменных из пришедших данных
+
+$name = $_POST['name'];
+
+$email = $_POST['email'];
+
+$tel = $_POST['tel'];
+
+$message = $_POST['message'];
+
+
+
+
+
+ 
+// Формируем сообщение для отправки, в нём мы соберём всё, что ввели в форме 
+
+$mes = "Имя: $name \nE-mail: $email \nТелефон: $tel \nТекст: $message";
+
+ 
+// Пытаемся отправить письмо по заданному адресу
+
+// Если нужно, чтобы письма всё время уходили на ваш адрес — замените первую переменную $email на свой адрес электронной почты
+
+$send = mail ("kaplin-web@yandex.ru",$tel,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+
+ 
+// Если отправка прошла успешно — так и пишем 
+
+if ($send == 'true')
+
+{echo "";}
+
+// Если письмо не ушло — выводим сообщение об ошибке
+
+else {echo "Ой, что-то пошло не так";}
+
+?>
